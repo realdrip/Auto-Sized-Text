772cce6d950b5646e27a399cbcea5fcff0fa4f89
Furkan Sayan <58613119+realdrip@users.noreply.github.com>
2026-05-11 06:13:32 +0300

    Update AutoSizeRichTextLabel.gd

--- addons/AutoSizeText/ui/AutoSizeRichTextLabel.gd
+++ addons/AutoSizeText/ui/AutoSizeRichTextLabel.gd
@@ -1,22 +1,8 @@
 @tool
-# # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Twister
-#
-# Following the context of the main AutoSize addon.
-# # # # # # # # # # # # # # # # # # # # # # # # # # #
-class_name AutoSizeRichTextLabel
-extends RichTextLabel
-
-## watch_text_change true is needed for this event to work
-signal text_changed(old_text: String, new_text: String)
-
-## When true, text-size will be re-calculated when the text is changed.[br]
-## Needed for the text_changed event to work.
-@export
-var watch_text_change: bool = false
+class_name AutoSizeRichTextLabel extends RichTextLabel
 
 @export_tool_button("FORCE REFRESH")
-var refresh_button: Callable = do_resize_text
+var refresh_button: Callable = RequestResizeText
 
 @export_group("Auto Font Size")
 
@@ -35,7 +21,7 @@ var min_font_size: int = 8:
 			)
 
 		notify_property_list_changed()
-		resize_text()
+		RequestResizeText()
 
 ## Max text size to reach
 @export_range(1, 512, 1.0)
@@ -52,7 +38,7 @@ var max_font_size: int = 38:
 			)
 
 		notify_property_list_changed()
-		resize_text()
+		RequestResizeText()
 
 @export_group("Font Step Size")
 
@@ -67,64 +53,49 @@ var step_sizes: Array[int] = []:
 		step_sizes.sort()
 		
 		notify_property_list_changed()
-		resize_text()
+		RequestResizeText()
+
 
 var _processing_flag: bool = false
-var _last_text: String = ""
-
 
 func _ready() -> void:
-	# TODO: change defaults instead of hard-setting!
+	force_default_settings()
 
+	AutoSizeTextRefresh.Register(self)
+	resized.connect(RequestResizeText)
+	RequestResizeText()
+
+func force_default_settings() -> void:
 	if autowrap_mode == TextServer.AUTOWRAP_OFF:
 		autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
-		push_warning("changed autowrap_mode to " + str(autowrap_mode))
 
-	resized.connect(do_resize_text)
-	do_resize_text()
+	if fit_content:
+		push_warning("Fit content can't be used (program freeze), setting it to false!")
+		fit_content = false
 
-
-func _process(_delta: float) -> void:
-	if watch_text_change:
-		if text != _last_text:
-			do_resize_text()
-			
-			if not Engine.is_editor_hint():
-				text_changed.emit(_last_text, text)
-			
-			_last_text = text
-
-
+func RequestResizeText() -> void:
+	call_deferred(&"do_resize_text")
+	
 func resize_text() -> void:
-	if not needs_resize():
-		return
-
-	do_resize_text()
-
+	RequestResizeText()
 
 func do_resize_text() -> void:
-	# Prevent draw call stack handler
 	if _processing_flag:
 		return 
 	
 	_processing_flag = true
-	
-	if fit_content:
-		push_warning("Fit content can't be used (program freeze), setting it to false!")
-		fit_content = false
-	
 	for target_font_size: int in get_iterator():
 		set(&"theme_override_font_sizes/bold_italics_font_size", target_font_size)
 		set(&"theme_override_font_sizes/italics_font_size", target_font_size)
 		set(&"theme_override_font_sizes/mono_font_size", target_font_size)
 		set(&"theme_override_font_sizes/normal_font_size", target_font_size)
 		set(&"theme_override_font_sizes/bold_font_size", target_font_size)
+		update_minimum_size()
 
-		if not get_content_height() > get_rect().size.y:
+		if not needs_resize():
 			break
 	
 	_processing_flag = false
-
 
 func get_iterator() -> Array:
 	if len(step_sizes) >= 2:
@@ -135,9 +106,7 @@ func get_iterator() -> Array:
 	if len(step_sizes) == 1:
 		push_warning(name + " Step sizes needs at least 2 numbers to work")
 	
-	return range(max_font_size, min_font_size, -1)	
-
+	return range(max_font_size, min_font_size - 1, -1)
 
 func needs_resize() -> bool:
-	# TODO: does this need a line-calculated get_line_offset?
 	return get_content_height() > get_rect().size.y
